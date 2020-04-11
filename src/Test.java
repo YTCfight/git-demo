7889f8b879c1c73386b0c68e7595752528529df9
yangtongchun <m15169658896@163.com>
2020-04-11 21:52:36 +0800

    第三次

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("ytc");
         System.out.println("haha");
+        System.out.println("第一次书写");
     }
 }
